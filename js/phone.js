e9013051f9584c1874c8c3353354cf59dbe51b85
Kasem32833 <abulkasem32833@gmail.com>
2023-08-31 12:55:56 +0300

    add dynamic search and display products

--- js/phone.js
+++ js/phone.js
@@ -1,21 +1,47 @@
-// function themeChanger(){
-//    const toggleBtn =  document.getElementById('theme-changer')
-//    if(toggleBtn.innerText == 'Dark'){
-//         toggleBtn.innerText = 'Light';
-//        const html = document.getElementsByName('html');
-//        html.setAttribute 
-//    }else if(toggleBtn.innerText == 'Light'){
-//         toggleBtn.innerText = 'Dark'
-//    }
 
-// }
 
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone#data/2')
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json();
-
-    const phone = (data.data)
-    console.log(phone[0]);
+    const phones = (data.data);
+    showPhones(phones);
+    console.log(phones);
 }
 
-loadPhone();
+function showPhones(phones){
+
+   
+    const phoneContainer = document.getElementById('phone-container');
+
+    // clear previous displayed item
+     phoneContainer.textContent = '';
+    
+
+    phones.forEach(phone => {
+      
+
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = 'card  bg-base-100 shadow-xl';
+        phoneCard.innerHTML = `
+        <figure><img src="${phone.image}" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">${phone.phone_name}</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+        `
+
+        phoneContainer.appendChild(phoneCard);
+        
+    });
+}
+
+function searchItems() {
+    console.log('search button clicked')
+    const searchText = document.getElementById('search-box').value;
+    console.log(searchText);
+    loadPhone(searchText);
+}
+
